036c857060840306f121befdf8cabb24bac1f76e
4nt-m4n <aguzman308@outlook.com>
2023-05-29 11:42:28 -0400

    Enemy class contains offset, added array list of enemies into main, added in level id to determine number of enemies. levelCleared method added to check if enemies list is empty.

--- src/Enemy.java
+++ src/Enemy.java
@@ -7,24 +7,33 @@ import java.util.ArrayList;
 
 public class Enemy {
     int health = 0;
+    long offset;
     float xPos;
     float yPos;
-    public Enemy(int health, float xPos, float yPos){
+    public Enemy(int health, float xPos, float yPos, long offset){
         this.health = health;
         this.xPos = xPos;
         this.yPos = yPos;
+        this.offset = offset;
     }
     int i = 0;
     public void draw(Raylib rlj, map map, float timer){
         ArrayList<Vector2> points = map.points;
+        //checks if stage has been reset
         if(timer == 0) {
             i = 0;
         }
-
-        if(i == points.size())
+        //if enemy reaches end of map
+        if(i == points.size()){
             return;
-        rlj.shapes.DrawCircleV(points.get(i), (float) 10.0, Color.RED);
-        i++;
+        }
+        //waits an iteration of the main while loop by the number determined from offset. ie: offset = 10, wait 10 iterations before drawing
+        if(offset == 0){
+            rlj.shapes.DrawCircleV(points.get(i), (float) 10.0, Color.RED);
+            i++;
+        }
+        else
+            offset--;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,44 @@
 import com.raylib.java.Raylib;
 import com.raylib.java.core.Color;
 
+import java.util.ArrayList;
+
 import static com.raylib.java.core.input.Keyboard.KEY_SPACE;
 
 public class Main {
+	static ArrayList<Enemy> enemies = new ArrayList<>();
 	public static void main(String[] args) {
 		
 		// window size, independent of actual screen size
 		final int screen_width = 960;
 		final int screen_height = 540;
-		float timer = 0.0f;
+		float timer = 0;
+		int level = 3;
+		long offset = 0;
 		Raylib rlj = new Raylib(screen_width, screen_height, "Tower Defense");
 		
 		map map = new map(screen_width, screen_height, screen_width / 5, 32);
 		//screenManager screenManager = new screenManager();
-		Enemy enemy = new Enemy(100, map.points.get(0).x, map.points.get(0).y);
+		//checks if the previous level is cleared. Increments level counter, and adds in a new set of enemies.
+		if(levelCleared()){
+			level++;
+			for(int i = 0; i < level; i++){
+				Enemy enemy = new Enemy(100, map.points.get(0).x, map.points.get(0).y, offset);
+				enemies.add(enemy);
+				offset += 10;
+			}
+		}
 		
 		rlj.core.SetTargetFPS(60);
 		
 		// main game loop- runs once every frame until window is closed with x button or ESC key
 		while (!rlj.core.WindowShouldClose()) {  // Detect window close button or ESC key
-			timer += 0.2f;
 			// regenerate map on space press
 			if (rlj.core.IsKeyReleased(KEY_SPACE)) {
 				map = new map(screen_width, screen_height, screen_width / 5, 32);
 				timer = 0;
 			}
-			
+			timer += 0.2f;
 			// Draw to screen here ------------------------------------------------------
 			rlj.core.BeginDrawing();
 			
@@ -37,13 +49,21 @@ public class Main {
 			rlj.shapes.DrawRectangle(0, 0, screen_width / 5, screen_height, Color.DARKGRAY);
 			rlj.text.DrawText("reserved for menu", 5, 0, 15, Color.RAYWHITE);
 			rlj.text.DrawText("press [space] to \ngenerate new map", 5, 50, 15, Color.RAYWHITE);
-			
+			rlj.text.DrawText("Level: " + level, 200, 5, 15, Color.RAYWHITE);
 			map.draw(rlj);
-			// create an enemy and have it run along the path
-			enemy.draw(rlj, map, timer);
+			//let enemies run along the path
+			for(int i = 0; i < enemies.size(); i++){
+				enemies.get(i).draw(rlj, map, timer);
+			}
 			//
 			rlj.core.EndDrawing();
 			//---------------------------------------------------------------------------
 		}
+	}
+	// method to check if enemies array list is empty.
+	public static boolean levelCleared(){
+		if(enemies.size() == 0)
+			return true;
+		return false;
 	}
 }
